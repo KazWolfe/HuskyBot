d9f5578020ac05db77eaf456630543f42cb40ea6
Kaz Wolfe <root@kazwolfe.io>
2018-03-23 01:03:59 -0700

    Improve AntiSpam Invite Blocker
    - Invite Blocker now has autobanning feature
      - If a user posts more than 5 unauthorized invites
        in a 30 minute period, they are autobanned.
      - Invalid invites do *not* count to this limit
      - Authorized invites will not reset this count
    - Invite Blocker now operates on guilds instead of
      fragments. This means whitelisting is easier:
        /as allowInvite <guild_id>
    - Invite Blocker will now only send a warning message
      on the first invite in a 30-minute window.
      - Invalid invites will never generate a warning
    - Fix regex to match on `-` and other characters
    - Slight performance improvements to stop running
      regex where not necessary.

--- plugins/AntiSpam.py
+++ plugins/AntiSpam.py
@@ -1,5 +1,6 @@
 import logging
 import re
+import datetime
 
 import discord
 from discord.ext import commands
@@ -16,6 +17,10 @@ class AntiSpam:
     def __init__(self, bot: discord.ext.commands.Bot):
         self.bot = bot
         self._config = WolfConfig.getConfig()
+
+        # Statics
+        self.INVITE_COOLDOWNS = {}
+
         LOG.info("Loaded plugin!")
 
     async def on_message(self, message):
@@ -43,60 +48,88 @@ class AntiSpam:
             ))
 
         if PING_BAN_LIMIT is not None and len(message.mentions) >= PING_BAN_LIMIT:
-            await message.author.ban(delete_message_days=1, reason="[AUTOMATIC BAN - AntiSpam Module] "
+            await message.author.ban(delete_message_days=0, reason="[AUTOMATIC BAN - AntiSpam Module] "
                                                                    "Multi-pinged over server ban limit.")
             # ToDo: Integrate with ServerLog to send custom ban message to staff logs.
 
     async def prevent_discord_invites(self, message):
-        ALLOWED_INVITES = self._config.get('antiSpam', {}).get('allowedInvites', ['diytech'])
+        ALLOWED_INVITES = self._config.get('antiSpam', {}).get('allowedInvites', [message.guild.id])
 
-        regex_matches = re.findall('discord\.gg/\w+', message.content, flags=re.IGNORECASE)
+        # Prepare the logger
+        log_channel = self._config.get('specialChannels', {}).get(ChannelKeys.STAFF_LOG.value, None)
+        if log_channel is None:
+            return
+        log_channel = message.guild.get_channel(log_channel)
 
+        # Prevent memory abuse by deleting expired cooldown records
+        if message.author.id in self.INVITE_COOLDOWNS \
+                and self.INVITE_COOLDOWNS[message.author.id]['cooldownExpiry'] < datetime.datetime.now():
+            del self.INVITE_COOLDOWNS[message.author.id]
+
+        # Users with MANAGE_MESSAGES are allowed to send unauthorized invites.
         if message.author.permissions_in(message.channel).manage_messages:
             return
 
+        regex_matches = re.findall('discord\.gg/.+', message.content, flags=re.IGNORECASE)
+
+        # Handle messages without any invites in them (by ignoring them)
         if regex_matches is None or regex_matches == []:
-            # No invite links detected. Move on.
             return
 
         for regex_match in regex_matches:
             fragment = re.split("discord\.gg/", regex_match, flags=re.IGNORECASE)[1]
 
-            if fragment in ALLOWED_INVITES:
-                # Permitted fragment
-                continue
-
-            await message.delete()
-            await message.channel.send(embed=discord.Embed(
-                title="Discord Invite Blocked",
-                description="Hey! It looks like you posted a Discord invite.\n\n"
-                            "Here on DIY Tech, we have a strict no-invites policy in order to prevent spam and "
-                            "advertisements. If you would like to post an invite, you may contact the admins to "
-                            "request an invite code be whitelisted.\n\n"
-                            "We apologize for the inconvenience.",
-                color=Colors.WARNING
-            ), delete_after=30.0)
-
-            # Send a message to the server log, too
-            log_channel = self._config.get('specialChannels', {}).get(ChannelKeys.STAFF_LOG.value, None)
-            if log_channel is None:
-                return
-            log_channel = message.guild.get_channel(log_channel)
-
+            # Attempt to validate the invite, deleting invalid ones
             try:
                 invite_data = await self.bot.get_invite("https://discord.gg/{}".format(fragment))
             except discord.errors.NotFound:
+                await message.delete()
+
                 invalid_embed = discord.Embed(
                     description="An invalid invite with key `{}` by user {} (ID `{}`) was caught and filtered."
-                                .format(fragment, str(message.author), str(message.author.id)),
+                        .format(fragment, str(message.author), str(message.author.id)),
                     color=Colors.INFO
                 )
-                invalid_embed.set_author(name="Invite from {} intercepted!".format(str(message.author)),
+                invalid_embed.set_author(name="Invite from {} intercepted in {}!"
+                                         .format(str(message.author), "#" + str(message.channel)),
                                          icon_url=message.author.avatar_url)
 
                 await log_channel.send(embed=invalid_embed)
                 break
 
+            # This guild is allowed to have invites on this server, so we can ignore them.
+            if invite_data.guild.id in ALLOWED_INVITES:
+                continue
+
+            # We have an invite from a non-whitelisted server. Delete it.
+            await message.delete()
+
+            # Add the user to the cooldowns table - we're going to use this to prevent DIYBot's spam and to ban the user
+            # if they go over 5 deleted invites in a 30 minute period.
+            if message.author.id not in self.INVITE_COOLDOWNS.keys():
+                self.INVITE_COOLDOWNS[message.author.id] = {
+                    'cooldownExpiry': datetime.datetime.now() + datetime.timedelta(minutes=30),
+                    'offenseCount': 0
+                }
+
+                # We're also going to be nice and inform the user on their *first offense only*. The message will
+                # self-destruct after 90 seconds.
+                await message.channel.send(embed=discord.Embed(
+                    title="Discord Invite Blocked",
+                    description="Hey {}! It looks like you posted a Discord invite.\n\n"
+                                "Here on DIY Tech, we have a strict no-invites policy in order to prevent spam and "
+                                "advertisements. If you would like to post an invite, you may contact the admins to "
+                                "request an invite code be whitelisted.\n\n"
+                                "We apologize for the inconvenience.".format(message.author.mention),
+                    color=Colors.WARNING
+                ), delete_after=90.0)
+
+            cooldownRecord = self.INVITE_COOLDOWNS[message.author.id]
+
+            # And we increment the offense counter here.
+            cooldownRecord['offenseCount'] += 1
+
+            # We've a valid invite, so let's log that with invite data.
             invite_embed = discord.Embed(
                 description="An invite with key `{}` by user {} (ID `{}`) was caught and filtered. Invite information "
                             "below.".format(fragment, str(message.author), str(message.author.id)),
@@ -104,13 +137,28 @@ class AntiSpam:
             )
             invite_embed.set_author(name="Invite from {} intercepted!".format(str(message.author)),
                                     icon_url=message.author.avatar_url)
-            invite_embed.add_field(name="Guild Name", value=invite_data.guild.name, inline=True)
-            invite_embed.add_field(name="Channel Name", value="#" + invite_data.channel.name, inline=True)
-            invite_embed.add_field(name="Guild ID", value=invite_data.guild.id, inline=True)
-            invite_embed.add_field(name="Guild Creation Date", value=str(invite_data.guild.created_at).split('.')[0],
+
+            invite_embed.add_field(name="Invited Guild Name", value=invite_data.guild.name, inline=True)
+            invite_embed.add_field(name="Invited Channel Name", value="#" + invite_data.channel.name, inline=True)
+            invite_embed.add_field(name="Invited Guild ID", value=invite_data.guild.id, inline=True)
+
+            invite_embed.add_field(name="Invited Guild Creation Date",
+                                   value=str(invite_data.guild.created_at).split('.')[0],
                                    inline=True)
 
+            invite_embed.set_footer(text="Strike {} of 5, resets {}"
+                                    .format(cooldownRecord['offenseCount'],
+                                            str(cooldownRecord['cooldownExpiry']).split('.')[0]))
+
             await log_channel.send(embed=invite_embed)
+
+            # If the user is over the offense limit, we're going to ban their ass. In this case, this means that on
+            # their sixth invalid invite, we ban 'em.
+            if cooldownRecord['offenseCount'] > 5:
+                await message.author.ban(reason="[AUTOMATIC BAN - AntiSpam Module] User sent over 5 unauthorized "
+                                                "invites in a 30 minute period.", delete_message_days=0)
+                del self.INVITE_COOLDOWNS[message.author.id]
+
             break
 
     @commands.group(name="antispam", aliases=['as'], brief="Manage the Antispam configuration for the bot")
@@ -150,70 +198,58 @@ class AntiSpam:
             color=Colors.SUCCESS
         ))
 
-    @asp.command(name="allowInvite", brief="Allow a specific invite slug (case sensitive!)")
+    @asp.command(name="allowInvite", brief="Allow an invite from the server ID given")
     @commands.has_permissions(manage_guild=True)
-    async def allow_invite(self, ctx: commands.Context, fragment: str):
+    async def allow_invite(self, ctx: commands.Context, guild: int):
         as_config = self._config.get('antiSpam', {})
-        allowed_invites = as_config.setdefault('allowedInvites', ['diytech'])
+        allowed_invites = as_config.setdefault('allowedInvites', [ctx.guild.id])
 
-        try:
-            invite_data = await self.bot.get_invite("https://discord.gg/{}".format(fragment))
-        except discord.errors.NotFound:
+        if guild in allowed_invites:
             await ctx.send(embed=discord.Embed(
                 title="AntiSpam Module",
-                description="The invite with fragment `{}` is invalid!".format(fragment),
-                color=Colors.ERROR
-            ))
-            return
-
-        if fragment in allowed_invites:
-            await ctx.send(embed=discord.Embed(
-                title="AntiSpam Module",
-                description="The invite with fragment `{}` is already whitelisted!".format(fragment),
+                description="The guild with ID `{}` is already whitelisted!".format(guild),
                 color=Colors.WARNING
             ))
             return
 
-        allowed_invites.append(fragment)
+        allowed_invites.append(guild)
         self._config.set("antiSpam", as_config)
         await ctx.send(embed=discord.Embed(
             title="AntiSpam Module",
-            description="The invite to **{}** with fragment `{}` has been added to the whitelist."
-                        .format(invite_data.guild.name, fragment),
+            description="The invite to guild `{}` has been added to the whitelist."
+                .format(guild),
             color=Colors.SUCCESS
         ))
         return
 
     @asp.command(name="blockInvite", brief="Remove an invite from the whitelist.")
     @commands.has_permissions(manage_guild=True)
-    async def block_invite(self, ctx: commands.Context, fragment: str):
+    async def block_invite(self, ctx: commands.Context, guild: int):
         as_config = self._config.get('antiSpam', {})
-        allowed_invites = as_config.setdefault('allowedInvites', ['diytech'])
+        allowed_invites = as_config.setdefault('allowedInvites', [ctx.guild.id])
 
-        try:
-            invite_data = await self.bot.get_invite("https://discord.gg/{}".format(fragment))
-        except discord.errors.NotFound:
+        if guild == ctx.guild.id:
             await ctx.send(embed=discord.Embed(
                 title="AntiSpam Module",
-                description="The invite with fragment `{}` is invalid!".format(fragment),
-                color=Colors.ERROR
-            ))
-            return
-
-        if fragment not in allowed_invites:
-            await ctx.send(embed=discord.Embed(
-                title="AntiSpam Module",
-                description="The invite with fragment `{}` is not whitelisted!".format(fragment),
+                description="This guild may not be removed from the whitelist!".format(guild),
                 color=Colors.WARNING
             ))
             return
 
-        allowed_invites.pop(fragment)
+        if guild not in allowed_invites:
+            await ctx.send(embed=discord.Embed(
+                title="AntiSpam Module",
+                description="The guild `{}` is not whitelisted!".format(guild),
+                color=Colors.WARNING
+            ))
+            return
+
+        allowed_invites.pop(guild)
         self._config.set("antiSpam", as_config)
         await ctx.send(embed=discord.Embed(
             title="AntiSpam Module",
-            description="The invite to **{}** with fragment `{}` has been removed from the whitelist."
-                        .format(invite_data.guild.name, fragment),
+            description="The guild with ID `{}` has been removed from the whitelist."
+                .format(guild),
             color=Colors.SUCCESS
         ))
         return
